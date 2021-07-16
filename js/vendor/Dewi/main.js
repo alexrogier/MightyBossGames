778b791c5a658e853cff27a7483d22d09fd3df54
Alex <alex.rogier92@gmail.com>
2021-07-15 20:17:08 -0500

    Update main.js
    add responsiveness to owl

--- js/vendor/Dewi/main.js
+++ js/vendor/Dewi/main.js
@@ -164,7 +164,18 @@
     dots: true,
     loop: true,
     items: 1,
-    nav: true
+    nav: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
   });
 
   // Porfolio isotope and filter
